2faf66b80ea15e28f65c8e99109ed2852272f232
AntorFR <contact@antor.fr>
2025-08-05 03:56:01 +0200

    Fix final code duplication and ESP8266 config
    - Remove duplicate code from __init__.py (fixes pylint R0801)
    - Fix ESP8266 WiFi config (remove secrets dependency)
    - Correct component path in ESP8266 example
    - All linting checks now pass

--- components/impulse_cover/__init__.py
+++ components/impulse_cover/__init__.py
@@ -1,54 +1,16 @@
+"""ESPHome Impulse Cover Component."""
+
 import esphome.codegen as cg
-from esphome.components import binary_sensor, cover, output
+from esphome.components import cover
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_CLOSE_DURATION,
-    CONF_ID,
-    CONF_OPEN_DURATION,
-    CONF_OUTPUT,
-)
 
 DEPENDENCIES = ["cover"]
 
-# Constants for configuration
-CONF_PULSE_DELAY = "pulse_delay"
-CONF_SAFETY_TIMEOUT = "safety_timeout"
-CONF_SAFETY_MAX_CYCLES = "safety_max_cycles"
-CONF_OPEN_SENSOR = "open_sensor"
-CONF_CLOSE_SENSOR = "close_sensor"
-CONF_OPEN_SENSOR_INVERTED = "open_sensor_inverted"
-CONF_CLOSE_SENSOR_INVERTED = "close_sensor_inverted"
-
-# Component namespace and class
+# Component namespace
 impulse_cover_ns = cg.esphome_ns.namespace("impulse_cover")
-ImpulseCover = impulse_cover_ns.class_("ImpulseCover", cover.Cover, cg.Component)
 
 
+# Empty to_code function - the actual implementation is in cover.py
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await cover.register_cover(var, config)
-
-    # Set required parameters
-    cg.add(var.set_open_duration(config[CONF_OPEN_DURATION]))
-    cg.add(var.set_close_duration(config[CONF_CLOSE_DURATION]))
-
-    # Set optional parameters
-    cg.add(var.set_pulse_delay(config[CONF_PULSE_DELAY]))
-    cg.add(var.set_safety_timeout(config[CONF_SAFETY_TIMEOUT]))
-    cg.add(var.set_safety_max_cycles(config[CONF_SAFETY_MAX_CYCLES]))
-
-    # Set output
-    output_var = await cg.get_variable(config[CONF_OUTPUT])
-    cg.add(var.set_output(output_var))
-
-    # Set sensors if provided
-    if CONF_OPEN_SENSOR in config:
-        open_sensor = await cg.get_variable(config[CONF_OPEN_SENSOR])
-        cg.add(var.set_open_sensor(open_sensor))
-        cg.add(var.set_open_sensor_inverted(config[CONF_OPEN_SENSOR_INVERTED]))
-
-    if CONF_CLOSE_SENSOR in config:
-        close_sensor = await cg.get_variable(config[CONF_CLOSE_SENSOR])
-        cg.add(var.set_close_sensor(close_sensor))
-        cg.add(var.set_close_sensor_inverted(config[CONF_CLOSE_SENSOR_INVERTED]))
+    """Empty to_code function - implementation in cover.py."""
+    pass
