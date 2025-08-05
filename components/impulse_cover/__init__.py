f02aa060bc0515364a520da2ca362e27176cefec
AntorFR <contact@antor.fr>
2025-08-05 03:58:02 +0200

    Achieve perfect pylint score (10.00/10)
    - Fix unnecessary pass statement warning
    - Fix unused argument warning with pylint disable
    - All linting tools now pass with perfect scores
    - Code quality at maximum level

--- components/impulse_cover/__init__.py
+++ components/impulse_cover/__init__.py
@@ -11,6 +11,5 @@ impulse_cover_ns = cg.esphome_ns.namespace("impulse_cover")
 
 
 # Empty to_code function - the actual implementation is in cover.py
-async def to_code(config):
+async def to_code(config):  # pylint: disable=unused-argument
     """Empty to_code function - implementation in cover.py."""
-    pass
